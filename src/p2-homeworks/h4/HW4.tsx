ed7aeacbcbf22bd84a25bb12dbf0182fc15aa7d4
KAST13L <o4enbsmeshno@gmail.com>
2022-06-21 16:21:55 +0300

    create local github.io host)))

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -18,7 +18,7 @@ function HW4() {
 
     const [checked, setChecked] = useState<boolean>(false)
     const testOnChange = (e: ChangeEvent<HTMLInputElement>) => setChecked(e.currentTarget.checked)
-
+    // first changed!!!
     return (
         <div>
             <hr/>
