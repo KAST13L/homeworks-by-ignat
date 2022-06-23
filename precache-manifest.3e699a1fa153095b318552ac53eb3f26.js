bf2b62c0b8d78cd0c87bfd708bfd3311b7c9287d
KAST13L <o4enbsmeshno@gmail.com>
2022-06-23 12:31:14 +0300

    Updates

---
+++ precache-manifest.3e699a1fa153095b318552ac53eb3f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb30a435b1960955eece70320d329f71",
+    "url": "/homeworks-by-ignat/index.html"
+  },
+  {
+    "revision": "0d4dde29358df79a1da0",
+    "url": "/homeworks-by-ignat/static/css/main.f2c1df2b.chunk.css"
+  },
+  {
+    "revision": "1406d2dc31283131779c",
+    "url": "/homeworks-by-ignat/static/js/2.cd4cc568.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homeworks-by-ignat/static/js/2.cd4cc568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4dde29358df79a1da0",
+    "url": "/homeworks-by-ignat/static/js/main.13bbb212.chunk.js"
+  },
+  {
+    "revision": "d6824a243215fa3682ff",
+    "url": "/homeworks-by-ignat/static/js/runtime-main.e13e2a19.js"
+  }
+]);
