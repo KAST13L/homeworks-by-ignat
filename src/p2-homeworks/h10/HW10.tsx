fcaa561bd943f42164b6ef6f653f4bf17ea60941
KAST13L <o4enbsmeshno@gmail.com>
2022-07-28 14:30:20 +0300

    10 hw is done

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -3,7 +3,6 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "./bll/store";
 import {loadingAC} from "./bll/loadingReducer";
-import preloader from '../../../common/preloader.svg'
 
 function HW10() {
     // useSelector, useDispatch
